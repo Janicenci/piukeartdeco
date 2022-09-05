5afe9f4cf0ba9938fc0ffbcc848f9be6158bad5a
Jani cenci <nunicenci@gmail.com>
2022-09-05 19:33:19 -0300

    animacion-gradiant

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
 document.addEventListener ('DOMContentLoaded', () => {
     const elementosCarousel = document.querySelectorAll ('.carousel');
     M.Carousel.init(elementosCarousel, {
-        duration: 150,
+        duration: 100,
+        dist: -80,
+        shift: 5,
+        padding: 5,
+        numVisible: 3,
+        indicators: true,
+        noWrap: false,
+       
     
     });
 });
